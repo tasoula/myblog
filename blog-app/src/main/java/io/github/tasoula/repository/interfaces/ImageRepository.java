1e5c1129d6055488fd5bfe6ab69623fdc565c0be
tasoula <nastiakorotkikh@yandex.ru>
2025-07-07 15:28:19 +0300

    Добавлено отображение картинок

--- blog-app/src/main/java/io/github/tasoula/repository/interfaces/ImageRepository.java
+++ blog-app/src/main/java/io/github/tasoula/repository/interfaces/ImageRepository.java
@@ -1,4 +1,7 @@
 package io.github.tasoula.repository.interfaces;
 
+import java.util.UUID;
+
 public interface ImageRepository {
+    String getPostImage(UUID postId);
 }
--- blog-app/src/main/java/io/github/tasoula/service/ImageService.java
+++ blog-app/src/main/java/io/github/tasoula/service/ImageService.java
@@ -1,7 +1,35 @@
 package io.github.tasoula.service;
 
+import io.github.tasoula.repository.interfaces.ImageRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
 @Service
 public class ImageService {
+    @Value("${upload.images.dir}") // свойство в application.properties
+    private String uploadDir;
+    private final ImageRepository repository;
+
+    public ImageService(ImageRepository repository) {
+        this.repository = repository;
+    }
+
+    public Optional<Resource> getImage(UUID postId) {
+        String fileName = repository.getPostImage(postId);
+        if (fileName == null) {
+            return Optional.empty();
+        }
+
+        Path filePath = Paths.get(uploadDir, fileName);
+        return Optional.of(new FileSystemResource(filePath.toFile()));
+    }
 }
